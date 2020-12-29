51b9683d48d6397557516b11fd0b9a8b4d3447fe
Boyan Yotov <boyan.yotov@abv.bg>
2020-12-29 14:42:34 +0000

    Separated Sense for Killer Cell

---
+++ CancerImmunologyGame/Assets/_Scripts/Cells/KillerSense.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Collider2D))]
+public class KillerSense : MonoBehaviour
+{
+	[SerializeField]
+	private KillerCell owner = null;
+
+	private List<CancerCell> cancerCellsInRange = new List<CancerCell>();
+	public List<CancerCell> CancerCellsInRange => cancerCellsInRange;
+
+
+	// Attack functionality
+	private void OnTriggerEnter2D(Collider2D collider)
+	{
+		CancerCellBody cell = collider.gameObject.GetComponent<CancerCellBody>();
+		Debug.Log(collider.gameObject.name);
+		if (cell != null)
+		{
+			cancerCellsInRange.Add(cell.owner);
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collider)
+	{
+		CancerCellBody cell = collider.gameObject.GetComponent<CancerCellBody>();
+		if (cell != null)
+		{
+			cancerCellsInRange.Remove(cell.owner);
+		}
+	}
+
+}
