4178332aa8a359bf670f17c0f2460ce194fd0827
Boyan Yotov <boyan.yotov@abv.bg>
2021-01-17 14:46:01 +0000

    Added Cellpedia items, descriptions and animations. Fixed few bugs.

---
+++ CancerImmunologyGame/Assets/_Scripts/UI/Cellpedia.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace CellpediaUI
+{
+	public class Cellpedia : Singleton<Cellpedia>
+	{
+		[Header("Link")]
+		[SerializeField]
+		private List<Petridish> petridishes = new List<Petridish>();
+		[SerializeField]
+		private List<CellDescriptionLink> cellDescriptions = new List<CellDescriptionLink>();
+		[SerializeField]
+		TMP_Text cellDescription = null;
+		[SerializeField]
+		TMP_Text cellName = null;
+		[Header("Attributes")]
+		private float time_between_shifts = 2.0f;
+
+		[Header("Debugging")]
+		[SerializeField]
+		private List<CellDescription> unlockedCellDescriptions = new List<CellDescription>();
+		[SerializeField]
+		private Dictionary<CellpediaCells, CellDescription> cellpediaDescriptions = new Dictionary<CellpediaCells, CellDescription>();
+		[SerializeField]
+		private int cdIndex = 0;
+		[SerializeField]
+		private int dishIndex = 0;
+		[SerializeField]
+		bool rotatorLocked = false;
+
+		void Start()
+		{
+			Initialise();
+		}
+
+		void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Z))
+			{
+				UnlockCellDescription(CellpediaCells.DENDRITIC);
+			}
+			if (Input.GetKeyDown(KeyCode.X))
+			{
+				UnlockCellDescription(CellpediaCells.REGULATORY);
+			}
+			if (Input.GetKeyDown(KeyCode.C))
+			{
+				UnlockCellDescription(CellpediaCells.CANCER);
+			}
+			if (Input.GetKeyDown(KeyCode.V))
+			{
+				UnlockCellDescription(CellpediaCells.THELPER);
+			}
+
+		}
+
+		public void Initialise()
+		{
+			Petridish.Timetopass = time_between_shifts;
+			foreach (CellDescriptionLink cdl in cellDescriptions)
+			{
+				cellpediaDescriptions.Add(cdl.cc, cdl.cd);
+			}
+
+			UnlockCellDescription(CellpediaCells.TKILLER);
+			petridishes[0].SetVisual(unlockedCellDescriptions[0]);
+		}
+
+		public void Close()
+		{
+			gameObject.SetActive(false);
+		}
+
+		public void Open()
+		{
+			gameObject.SetActive(true);
+		}
+
+		public void UnlockCellDescription(CellpediaCells cc)
+		{
+			unlockedCellDescriptions.Add(cellpediaDescriptions[cc]);
+
+			// Make button glow!
+		}
+
+		public void NextPetridish()
+		{
+			if (unlockedCellDescriptions.Count <= 1) return;
+
+			cdIndex++;
+			cdIndex %= unlockedCellDescriptions.Count;
+			dishIndex ^= 1;
+
+			if (!rotatorLocked)
+			{
+				rotatorLocked = true;
+				var cd = unlockedCellDescriptions[cdIndex];
+				petridishes[dishIndex].SetVisual(cd);
+				petridishes[0].ShiftLeft();
+				petridishes[1].ShiftLeft();
+				cellDescription.text = cd.description;
+				cellName.text = cd.cellname;
+
+				StartCoroutine(WaitForRotator());
+			}
+		}
+
+		private IEnumerator WaitForRotator()
+		{
+			yield return new WaitForSeconds(time_between_shifts);
+			rotatorLocked = false;
+		}
+
+		[System.Serializable]
+		public struct CellDescriptionLink
+		{
+			public CellDescription cd;
+			public CellpediaCells cc;
+		}
+	}
+}
---
+++ CancerImmunologyGame/Assets/_Scripts/UI/Petridish.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CellpediaUI
+{
+	public class Petridish : MonoBehaviour
+	{
+		private static Petridish nextPetridish = null;
+
+		[Header("Links")]
+		[SerializeField]
+		private RectTransform trans = null;
+		[SerializeField]
+		private RectTransform parentTransform = null;
+		[SerializeField]
+		private GameObject cellVisual = null;
+		[SerializeField]
+		private Animator cellVisualAnimator;
+		[SerializeField]
+		private Image cellVisualSprite = null;
+
+		[Header("Attributes")]
+		[SerializeField]
+		private bool isLeaving = false;
+		[SerializeField]
+		private static float timetopass = 2.0f;
+
+		[Header("Debugging")]
+		[SerializeField]
+		private float xStartCoord = 0.0f;
+		[SerializeField]
+		private static float xShiftCoord = 0.0f;
+		[SerializeField]
+		private bool isShifting = false;
+		[SerializeField]
+		private float timepassed = 0.0f;
+
+		public static float Timetopass { get => timetopass; set => timetopass = value; }
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			xShiftCoord = parentTransform.rect.width;
+			if (isLeaving)
+			{
+				xStartCoord = 0.0f;
+			}
+			else
+			{
+				xStartCoord = xShiftCoord;
+				nextPetridish = this;
+			}
+			trans.localPosition = new Vector2(xStartCoord, 0.0f);
+		}
+
+
+		// Update is called once per frame
+		void Update()
+		{
+			if (!isShifting) return;
+
+			if (isShifting && timepassed < Timetopass)
+			{
+				timepassed += Time.deltaTime;
+				if (timepassed > Timetopass)
+					timepassed = Timetopass;
+
+
+				trans.localPosition = new Vector2(xStartCoord - (timepassed / Timetopass) * xShiftCoord, 0.0f);
+				return;
+			}
+
+			Reset();
+		}
+
+		private void Reset()
+		{
+			isShifting = false;
+			timepassed = 0.0f;
+
+			if (isLeaving)
+			{
+				xStartCoord = xShiftCoord;
+				nextPetridish = this;
+			}
+			else
+			{
+				xStartCoord = 0.0f;
+			}
+			isLeaving = !isLeaving;
+			trans.localPosition = new Vector2(xStartCoord, 0.0f);
+		}
+
+		internal void ShiftLeft()
+		{
+			timepassed = 0.0f;
+			isShifting = true;
+		}
+
+		internal void SetVisual(CellDescription cd)
+		{
+			Debug.Log("Setting visual for: " + gameObject.name);
+			float scaleValue = 0.5f * cd.scale;
+			cellVisual.transform.localScale = new Vector3(scaleValue, scaleValue, 1.0f);
+			//cellVisualAnimator.SetTrigger(cd.animatorTrigger);
+			cellVisualAnimator.Play(cd.animatorTrigger);
+			cellVisualSprite.sprite = cd.sprite;
+		}
+
+	}
+}
