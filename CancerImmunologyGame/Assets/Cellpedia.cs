551503a4339f6f4f83e904f849d13a41e4a0d9a6
Boyan Yotov <boyan.yotov@abv.bg>
2021-01-17 13:11:51 +0000

    Added first design of Cellpedia.

---
+++ CancerImmunologyGame/Assets/Cellpedia.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Cellpedia : Singleton<Cellpedia>
+{
+
+	[SerializeField]
+	List<Petridish> petridishes = new List<Petridish>();
+	[SerializeField]
+	Button rotatorBtn = null;
+	bool rotatorLocked = false;
+
+	[SerializeField]
+	List<string> texts = new List<string>();
+	int textIndex = 0;
+
+	[SerializeField]
+	TMP_Text text = null;
+
+	public void Close()
+	{
+		gameObject.SetActive(false);
+	}
+
+	public void Open()
+	{
+		gameObject.SetActive(true);
+	}
+
+	public void NextPetridish()
+	{
+		if (!rotatorLocked)
+		{
+			rotatorLocked = true;
+			foreach (Petridish dish in petridishes)
+			{
+				dish.ShiftLeft();
+			}
+			StartCoroutine(WaitToUnlock());
+
+		}
+	}
+
+	IEnumerator WaitToUnlock()
+	{
+		yield return new WaitForSeconds(2.2f);
+
+		textIndex++;
+		textIndex %= texts.Count;
+		text.text = texts[textIndex];
+
+		rotatorLocked = false;
+	}
+
+
+}
---
+++ CancerImmunologyGame/Assets/Petridish.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Petridish : MonoBehaviour
+{
+	private static Petridish nextPetridish = null;
+
+	[Header("Links")]
+	[SerializeField]
+	private RectTransform trans = null;
+	[SerializeField]
+	private RectTransform parentTransform = null;
+
+	[Header("Attributes")]
+	[SerializeField]
+	private bool isLeaving = false;
+	[SerializeField]
+	private static float timetopass = 2.0f;
+
+	[Header("Debugging")]
+	[SerializeField]
+	private float xStartCoord = 0.0f;
+	[SerializeField]
+	private static float xShiftCoord = 0.0f;
+	[SerializeField]
+	private bool isShifting = false;
+	[SerializeField]
+	private float timepassed = 0.0f;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		xShiftCoord = parentTransform.rect.width;
+		if (isLeaving)
+		{
+			xStartCoord = 0.0f;
+		} else
+		{
+			xStartCoord = xShiftCoord;
+			nextPetridish = this;
+		}
+		trans.localPosition = new Vector2(xStartCoord, 0.0f);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (!isShifting) return;
+
+        if (isShifting && timepassed < timetopass)
+		{
+			timepassed += Time.deltaTime;
+			if (timepassed > timetopass)
+				timepassed = timetopass;
+
+
+			trans.localPosition = new Vector2(xStartCoord - (timepassed / timetopass) * xShiftCoord, 0.0f);
+			return;
+		}
+
+		Reset();
+	}
+
+	public void Reset()
+	{
+		isShifting = false;
+		timepassed = 0.0f;
+	
+		if (isLeaving)
+		{
+			isLeaving = false;
+			trans.localPosition = new Vector2(xShiftCoord, 0.0f);
+			nextPetridish = this;
+		}
+		else
+		{
+			isLeaving = true;
+		}
+	}
+
+	public void ShiftLeft()
+	{
+		timepassed = 0.0f;
+		isShifting = true;
+	}
+
+}
