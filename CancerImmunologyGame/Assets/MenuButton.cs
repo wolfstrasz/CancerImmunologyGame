d2fe600f68051b64de503776263219afd01ef493
Boyan Yotov <boyan.yotov@abv.bg>
2021-01-24 13:06:41 +0000

    Added new UI with button sounds

---
+++ CancerImmunologyGame/Assets/MenuButton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+
+public class MenuButton : MonoBehaviour, ISelectHandler, IPointerEnterHandler, IPointerExitHandler
+{
+	[SerializeField]
+	private AudioSource audioSource = null;
+	[SerializeField]
+	private Vector3 scaling = new Vector3(1.0f, 1.0f, 1.0f);
+
+	// When highlighted with mouse.
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		// Do something.
+		audioSource.Play();
+		gameObject.transform.localScale = scaling;
+		Debug.Log("<color=red>Event:</color> Completed mouse highlight.");
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		gameObject.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+	}
+
+	// When selected.
+	public void OnSelect(BaseEventData eventData)
+	{
+		// Do something.
+		audioSource.Play();
+		gameObject.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+		Debug.Log("<color=red>Event:</color> Completed selection.");
+	}
+}
